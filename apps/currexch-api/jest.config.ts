7972e5d06231484b62fac64ce9ab23ed04e8ad09
Prokopuk Pavel <pashaprok23@gmail.com>
2022-05-25 11:56:08 +0300

    initial commit

---
+++ apps/currexch-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'currexch-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/currexch-api',
+};
